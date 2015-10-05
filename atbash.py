7f5c0417d5bcacf10a06263e4d1d72f98f337d36
Marco Antonio Islas Cruz <markuz@islascruz.org>
2015-10-04 21:55:58 -0500

    Agregado el credito a Pedro Jesús González Vargas

--- atbash.py
+++ atbash.py
@@ -1,3 +1,4 @@
+#Escrito originalmente por Pedro Jesús González Vargas 
 def contar(cad):
     abc = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
     o=list(abc)
