3b25c088be527513fbfd5ca72a5adb52df348b52
Marco Antonio Islas Cruz <markuz@islascruz.org>
2015-10-04 22:03:22 -0500

    Unos cambios básicos

--- atbash.py
+++ atbash.py
@@ -1,13 +1,29 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# Hay que poner la linea shebang https://en.wikipedia.org/wiki/Shebang_(Unix)
+# Coding y encoding son Necesarios para poder hacer uso de hacentos y otros 
+#  caracteres no ascii.
+
 #Escrito originalmente por Pedro Jesús González Vargas 
 def contar(cad):
-    abc = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    # Esto lo podemos sustituir por string.letters
+    #  import string
+    #  string.letters
+    # Debemos respetar el PEP-8 y no escribir mas de 80 caracteres
+    #  https://www.python.org/dev/peps/pep-0008/
+    abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+            "R","S","T","U","V","W","X","Y","Z"]
+    # abc lo pudimios inicializar como una lista en lugar de una tupla.
     o=list(abc)
     o.reverse()
     b={}
     for i in range(len(abc)):
         b[str(abc[i])]=i
+    result = ""
     for i in cad:
-        print o[int(b[i])]
+        result += o[int(b[i])]
+    print result
 contar("KVWIL")
     
     
