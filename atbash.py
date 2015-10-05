a9bc5cd8698887330155f6e0103f89caaae8bebb
Marco Antonio Islas Cruz <markuz@islascruz.org>
2015-10-04 21:54:40 -0500

    Added atbash.py

---
+++ atbash.py
@@ -0,0 +1,14 @@
+def contar(cad):
+    abc = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    o=list(abc)
+    o.reverse()
+    b={}
+    for i in range(len(abc)):
+        b[str(abc[i])]=i
+    for i in cad:
+        print o[int(b[i])]
+contar("KVWIL")
+    
+    
+ 
+
